81d75413cfc2312cbb8c7d5c8c86e326607629bc
Rzhevsky-forever <egorbarisovi4@gmail.com>
2019-05-27 00:57:41 +0500

    create migrations from questions table

---
+++ database/migrations/2019_05_26_164940_create_questions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title', 100); // VARCHAR (! CHAR)
+            $table->text('text');
+            $table->string('short_desc', 250); // VARCHAR (! CHAR)
+            $table->integer('users_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
